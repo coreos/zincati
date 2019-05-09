102c2d9b70555a6efb1ebc437a0563418c6d3b02
Luca Bruno <luca.bruno@coreos.com>
2019-05-09 09:54:17 +0000

    zincati: add basic CLI skeleton

---
+++ src/cli/mod.rs
@@ -0,0 +1,35 @@
+//! Command-line interface (CLI) logic.
+
+use log::LevelFilter;
+use structopt::StructOpt;
+
+/// CLI configuration options.
+#[derive(Debug, StructOpt)]
+pub(crate) struct CliOptions {
+    /// Verbosity level (higher is more verbose).
+    #[structopt(short = "v", parse(from_occurrences), raw(global = "true"))]
+    verbosity: u8,
+
+    /// CLI sub-command.
+    #[structopt(subcommand)]
+    pub(crate) cmd: CliCommand,
+}
+
+impl CliOptions {
+    /// Returns the log-level set via command-line flags.
+    pub(crate) fn loglevel(&self) -> LevelFilter {
+        match self.verbosity {
+            0 => LevelFilter::Warn,
+            1 => LevelFilter::Info,
+            2 => LevelFilter::Debug,
+            _ => LevelFilter::Trace,
+        }
+    }
+}
+
+/// CLI sub-commands.
+#[derive(Debug, StructOpt)]
+pub(crate) enum CliCommand {
+    #[structopt(name = "agent")]
+    Agent,
+}
