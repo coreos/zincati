fb50f661af3d8739e3776d7a3412cee74ec658e9
Luca Bruno <luca.bruno@coreos.com>
2019-05-28 09:59:36 +0000

    zincati: rustfmt whole project

--- src/cincinnati/mock_tests.rs
+++ src/cincinnati/mock_tests.rs
@@ -7,10 +7,7 @@ use tokio::runtime::current_thread as rt;
 fn test_empty_graph() {
     let empty_graph = r#"{ "nodes": [], "edges": [] }"#;
     let m_graph = mockito::mock("GET", Matcher::Regex(r"^/v1/graph?.+$".to_string()))
-        .match_header(
-            "accept",
-            Matcher::Regex("application/json".to_string()),
-        )
+        .match_header("accept", Matcher::Regex("application/json".to_string()))
         .with_body(&empty_graph)
         .with_status(200)
         .create();
